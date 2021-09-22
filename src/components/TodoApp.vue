f1941e4e2834c7949feb16cbcf37f7216daee81d
LeHamzaSh <hrsheikh05@gmail.com>
2021-09-22 18:25:23 +0400

    Final Commit
    Added bootstrap css to status properties, added a line through for completed task.

--- src/components/TodoApp.vue
+++ src/components/TodoApp.vue
@@ -22,13 +22,19 @@
   <tbody>
     <tr v-for="(task, index) in tasks" :key="index">
       <!-- Display the name of the Task -->
-      <td>{{ task.name }}</td>
+      <td><span :class="{'completedItem': task.status === 'Completed'}">{{ task.name }}</span></td>
 
       <!-- Display the status of the Task -->
-      <td>{{ task.status }}</td>
+      <!-- The Statuses are clickable to change -->
+      <td style="width: 140px"><span @click="statusChange(index)" class="pointer" 
+      :class="{'text-danger': task.status === 'To-do',
+      'text-warning': task.status === 'In-progress',
+      'text-success': task.status === 'Completed'}">
+        {{ task.status }}
+        </span></td>
 
       <td>
-        <div class="text-center">
+        <div class="text-center" @click="editItem(index)">
           <span class="fa fa-pen"></span>
         </div>
       </td>
@@ -55,16 +61,12 @@ export default {
   data(){
     return{
       task: '',
-      tasks:[
-        {
-          name: 'Play Ping Pong.',
-          status: 'to-do'
-        },
-        {
-          name: 'Take out Trash',
-          status: 'in-progress'
-        }
-      ] 
+      editedItem: null,
+      //update statuses array
+      statusAvailabile: ['To-do', 'In-progress', 'Completed'],
+
+      //Store items in the task array
+      tasks:[] 
     }
   },
 
@@ -73,10 +75,17 @@ export default {
       if(this.task.length === 0){
         return alert('It cannot be empty!!')
       }
+      if(this.editedItem === null){
       this.tasks.push({
         name: this.task,
-        status: 'to-do'
+        status: 'To-do'
       })
+      }
+
+      else{
+        this.tasks[this.editedItem].name = this.task;
+        this.editedItem = null;
+      }
 
       // Reset the Enter Task after entering data
       this.task = '';
@@ -85,6 +94,19 @@ export default {
     deleteItem(index){
       /* Remove the Item from the array */
       this.tasks.splice(index, 1) // Remove only 1 item
+    },
+
+    editItem(index){
+      this.task = this.tasks[index].name
+      this.editedItem = index
+
+    },
+
+    statusChange(index){
+      //Loop over the statuses on each click
+      let indexTwo  =this.statusAvailabile.indexOf(this.tasks[index].status);
+      if(++indexTwo > 2) indexTwo = 0
+      this.tasks[index].status = this.statusAvailabile[indexTwo]
     }
   }
 }
@@ -92,4 +114,12 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.pointer{
+  cursor: pointer;
+}
+
+.completedItem {
+  text-decoration: line-through;
+
+}
 </style>
